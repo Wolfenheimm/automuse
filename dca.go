f9f87a9e135b8b2acff01dfbf63771890355d392
Jesse Rafters <jesse.rafters@gmail.com>
2024-08-13 00:18:14 -0400

    fixed the buffered frames to 10000 because songs were ending early

--- dca.go
+++ dca.go
@@ -3,49 +3,69 @@ package main
 import (
 	"io"
 	"log"
+	"strings"
 
 	"github.com/jonas747/dca"
 )
 
 // Encodes the video for audio playback
 func (v *VoiceInstance) DCA(path string, isMpeg bool) {
+	log.Println("INFO: Streaming from URL:", v.nowPlaying.VideoURL)
+	log.Println("INFO: Starting DCA function")
+
 	var encodeSession *dca.EncodeSession
 	var err error
 
 	if isMpeg {
 		dirPath := "mpegs/" + path
+		log.Println("INFO: Encoding MPEG file at path:", dirPath)
 		encodeSession, err = dca.EncodeFile(dirPath, opts)
-		defer encodeSession.Cleanup()
 	} else {
+		log.Println("INFO: Encoding file at path:", path)
 		encodeSession, err = dca.EncodeFile(path, opts)
-		defer encodeSession.Cleanup()
 	}
 
 	if err != nil {
 		log.Println("FATA: Failed creating an encoding session: ", err)
 		return
 	}
+	defer encodeSession.Cleanup()
 
 	v.encoder = encodeSession
 	done := make(chan error)
 	stream := dca.NewStream(encodeSession, v.voice, done)
 	v.stream = stream
+
+	log.Println("INFO: DCA stream started")
+
 	dcaErr := <-done
-	if dcaErr != nil && dcaErr != io.EOF {
-		log.Println("DCA stopped suddenly: ", dcaErr)
+	log.Println("INFO: DCA stream finished")
+
+	if dcaErr != nil {
+		log.Println("ERROR: DCA stream error: ", dcaErr) // Log the error message
+
+		if dcaErr == io.EOF {
+			log.Println("INFO: DCA stream ended normally with EOF")
+		} else if strings.Contains(dcaErr.Error(), `strconv.ParseFloat: parsing "N": invalid syntax`) {
+			log.Println("WARN: Invalid duration detected, setting duration to 0 to prevent crash.")
+			v.nowPlaying.Duration = "0"
+		} else {
+			log.Println("DCA stopped suddenly: ", dcaErr)
+		}
 		v.stream = nil
+	} else {
+		log.Println("INFO: DCA stream ended without error")
 	}
 }
 
 func setUpDcaOptions() {
-	opts.RawOutput = true
-	opts.Bitrate = 96
+	opts.RawOutput = false
+	opts.Bitrate = 128
 	opts.Application = "lowdelay"
 	opts.Volume = 100
 	opts.CompressionLevel = 10
 	opts.FrameDuration = 20
 	opts.PacketLoss = 1
-	opts.RawOutput = true
 	opts.VBR = true
-	opts.BufferedFrames = 100
+	opts.BufferedFrames = 10000
 }
